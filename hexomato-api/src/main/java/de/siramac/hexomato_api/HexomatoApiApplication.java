b12991b84772d8148238ef74a4d18dee1354f5f0
Dennis Winter <dennis.winter@mail.de>
2024-06-27 15:30:13 +0200

    split into hexomato and hexomato-api

---
+++ hexomato-api/src/main/java/de/siramac/hexomato_api/HexomatoApiApplication.java
@@ -0,0 +1,13 @@
+package de.siramac.hexomato_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexomatoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexomatoApiApplication.class, args);
+	}
+
+}
---
+++ hexomato-api/src/test/java/de/siramac/hexomato_api/HexomatoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.siramac.hexomato_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexomatoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
